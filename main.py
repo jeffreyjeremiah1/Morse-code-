491a772fd1e0ae6c4d3ab76d52a073b0aede46cc
Jeffrey Jeremiah <83784485+jeffreyjeremiah1@users.noreply.github.com>
2022-12-22 17:08:14 +0200

    added decoding function

--- main.py
+++ main.py
@@ -1,71 +1,76 @@
-morse_dict = {'a': '.- ',
-              'b': '-... ',
-              'c': '-.-. ',
-              'd': '-.. ',
-              'e': '. ',
-              'f': '--. ',
-              'g': '--. ',
-              'h': '.... ',
-              'i': '.. ',
-              'j': '.--- ',
-              'k': '-.- ',
-              'l': '.-.. ',
-              'm': '-- ',
-              'n': '-. ',
-              'o': '--- ',
-              'p': '.--. ',
-              'q': '--.- ',
-              'r': '.-. ',
-              's': '... ',
-              't': '- ',
-              'u': '..- ',
-              'v': '...- ',
-              'w': '.-- ',
-              'x': '-..- ',
-              'y': '-.-- ',
-              'z': '--.. ',
-              '0': '----- ',
-              '1': '.---- ',
-              '2': '..--- ',
-              '3': '...-- ',
-              '4': '....- ',
-              '5': '..... ',
-              '6': '-.... ',
-              '7': '--... ',
-              '8': '---.. ',
-              '9': '----. ',
-              '&': '.-... ',
-              "'": '.----. ',
-              '@': '.--.-. ',
-              ')': '-.--.- ',
-              '(': '-.--. ',
-              ':': '---... ',
-              ',': '--..-- ',
-              '=': '-...- ',
-              '!': '-.-.-- ',
-              '.': '.-.-.- ',
-              '-': '-....- ',
-              '+': '.-.-. ',
-              '"': '.-..-. ',
-              '?': '..--.. ',
-              '/': '-..-. ',
-              ' ': ' / '
+morse_dict = {'a': '.-',
+              'b': '-...',
+              'c': '-.-.',
+              'd': '-..',
+              'e': '.',
+              'f': '..-.',
+              'g': '--.',
+              'h': '....',
+              'i': '..',
+              'j': '.---',
+              'k': '-.-',
+              'l': '.-..',
+              'm': '--',
+              'n': '-.',
+              'o': '---',
+              'p': '.--.',
+              'q': '--.-',
+              'r': '.-.',
+              's': '...',
+              't': '-',
+              'u': '..-',
+              'v': '...-',
+              'w': '.--',
+              'x': '-..-',
+              'y': '-.--',
+              'z': '--..',
+              '0': '-----',
+              '1': '.----',
+              '2': '..---',
+              '3': '...--',
+              '4': '....-',
+              '5': '.....',
+              '6': '-....',
+              '7': '--...',
+              '8': '---..',
+              '9': '----.',
+              '&': '.-...',
+              "'": '.----.',
+              '@': '.--.-.',
+              ')': '-.--.-',
+              '(': '-.--.',
+              ':': '---...',
+              ',': '--..--',
+              '=': '-...-',
+              '!': '-.-.--',
+              '.': '.-.-.-',
+              '-': '-....-',
+              '+': '.-.-.',
+              '"': '.-..-.',
+              '?': '..--..',
+              '/': '-..-.',
+              ' ': '/'
               }
 
 
-def translate():
-    morse_code = []
-    list_of_characters = []
-    character_input = input("Enter any characters here: ").lower()
-
-    for char in character_input:
-        list_of_characters.append(char)
-    for item in list_of_characters:
-        if item in morse_dict:
-            morse_code.append(morse_dict[item])
-        else:
-            print(f"{item} is not in the morse dictionary")
-    print(str(morse_code).strip('[]').replace('\'', ''))
+def encode_morse(character_input):
+    morse_code = ""
+    for char in character_input.lower():
+        if char in morse_dict:
+            morse_code += morse_dict[char] + ' '
+    print(morse_code)
+    return morse_code
 
 
-translate()
+def decode_morse(morse_code):
+    text = ' '
+    for code in morse_code.split():
+        for key, val in morse_dict.items():
+            if code == val:
+                text += key
+    print(text)
+    return text
+
+
+encode_morse('food')
+decode_morse('..-. --- --- -..')
